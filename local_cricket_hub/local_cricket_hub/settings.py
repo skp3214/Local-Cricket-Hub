aa8e4d11ef2a92d89d8fbb1f7db033426d220837
Sachin Prajapati <skprajapati3214@gmail.com>
2025-04-05 13:53:14 +0530

    refactor settings; remove HTTPS from ALLOWED_HOSTS for consistency

--- local_cricket_hub/local_cricket_hub/settings.py
+++ local_cricket_hub/local_cricket_hub/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://local-cricket-hub.onrender.com','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['local-cricket-hub.onrender.com','127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://local-cricket-hub.onrender.com',
